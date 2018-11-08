71ff7dc0e8f0f9f3ff4acac2c4b862db9bc26cbf
gabrielpallotta <gabriel.cardenete@hotmail.com>
2018-11-08 08:27:46 -0200

    Add command line interface to add/remove values from btree

--- main.cpp
+++ main.cpp
@@ -1,31 +1,64 @@
 #include <iostream>
+#include <stdexcept>
 
 #include "Tree.hpp"
 
 using namespace std;
 
+#ifdef _WIN32
+  #define CLEAR_CMD "cls"
+#elif __linux__
+  #define CLEAR_CMD "clear"
+#endif
+
 int main()
 {
-  Tree<int> tree = Tree<int>(3);
+  cout << "Digite o n da arvore: ";
 
-  tree.add(1);
-  cout << tree << endl;
-  tree.add(7);
-  cout << tree << endl;
-  tree.add(4);
-  cout << tree << endl;
-  tree.add(2);
-  cout << tree << endl;
-  tree.add(5);
-  cout << tree << endl;
-  tree.add(9);
-  cout << tree << endl;
-  tree.remove(4);
-  cout << tree << endl;
-  tree.remove(5);
-  cout << tree << endl;
-  tree.remove(9);
-  cout << tree << endl;
+  int n;
 
-  return 0;
+  cin >> n;
+
+  Tree<int> tree = Tree<int>(n);
+
+  while (true) {
+    system(CLEAR_CMD);
+    cout << "Arvore Naria:" << endl;
+    cout << tree << endl;
+    cout << "Comandos disponiveis: " << endl << endl;
+    cout << "1 - Inserir" << endl;
+    cout << "2 - Remover" << endl << endl;
+    cout << "Digite uma opcao: ";
+
+    int option;
+    cin >> option;
+
+    int value;
+    switch (option) {
+      case 1:
+        cout << "Digite o valor que deseja inserir: ";
+        cin >> value;
+        try {
+          tree.add(value);
+          cout << "Valor inserido com sucesso" ;
+        } catch(exception &err) {
+          cout << "Valor ja existe na arvore";
+        }
+        break;
+      case 2:
+        cout << "Digite o valor que deseja remover: ";
+        cin >> value;
+        try {
+          tree.remove(value);
+          cout << "Valor removido com sucesso";
+        } catch(exception &err) {
+          cout << "Valor nao existe na arvore";
+        }  
+        break;  
+    }
+    cout << endl << endl;
+    cout << "Pressione qualquer tecla para continuar...";
+    cin.sync();
+    cin.ignore();
+  }
 }
